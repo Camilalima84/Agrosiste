374467bff421aca024dc57105e4298c1f2be180e
Antonio Evandro <100598553+AntonioEvandro@users.noreply.github.com>
2023-06-22 21:34:49 -0300

    Novas dep

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { Login } from './pages/login'
+import { Express } from "express";
+
+//SQL middleware
+App.use(express.sql());
 
 function App() {
   const router = createBrowserRouter([
